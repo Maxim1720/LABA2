10895883d2b1ab194b3f4c08655bd244a95235fb
Максим <56740030+Maxim1720@users.noreply.github.com>
2019-12-22 23:31:15 +0400

    Update laba2_7.cpp

--- laba2_7.cpp
+++ laba2_7.cpp
@@ -1,34 +1,21 @@
-﻿#include <iostream>
 #include<stdio.h>
-#include<stdlib.h>
-#include<locale.h>
-
-long double fact(int n) {
-	if (n == 0 || n == 1)
-		return 1;
-	else
-		return n * fact(n - 1);
-}
-
+#include<math.h>
 int main()
 {
-	setlocale(LC_ALL, "russian");
-	int n = 1;
-	float x, s, summa=0;
-	double eps = 0.00001;
-	printf("Введите значение переменной х = ");
-	scanf("%f", &x);
-	
-	do {
-		s = (powf(x, n) / fact(n));
-		summa = summa + s;
-		n++;
-		n++;
-	} 
-	while (fabs(s) >= eps); {
-
-		printf("f(x)=%.5f\n", summa);
-		printf("shx=%.5lf", sinh(x));
+	unsigned int n = 1;
+	double x;
+	double eps = 0.0001;
+	printf("print x = ");
+	scanf_s("%lf", &x);
+	long double s = x;
+	long double sum = s;
+	for(; fabs(s)>eps; n++)								//while (fabs(s) > eps)
+	{
+		s*= x * x / (2. * n + 1.) / (2. * n);
+		sum += s;
 	}
+
+	printf("f.(x)=%lf\n",sum);//результат
+	printf("sh(x)=%lf", sinh(x));//проверка
 	return 0;
 }
